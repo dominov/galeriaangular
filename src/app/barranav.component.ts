0df369d1676e34e8ab28c075e58c9adf482b1e30
Cristian Pardo (MAC) <pardocrespocristian@gmail.com>
2017-10-11 18:00:13 -0500

    iniciar Barra

---
+++ src/app/barranav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cp-barranav',
+  templateUrl: './barranav.component.html',
+  styles: []
+})
+export class BarranavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
